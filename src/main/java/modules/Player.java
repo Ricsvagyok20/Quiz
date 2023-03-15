efe75f2b67a18f72cb9c6437bf489362fc1c3253
Ruck Richard <h163500@stud.u-szeged.hu>
2023-03-15 16:07:14 +0100

    Kezdetleges modul es Dao alapok

---
+++ src/main/java/modules/Player.java
@@ -0,0 +1,11 @@
+package modules;
+
+import lombok.Data;
+@Data
+class Player{
+    public String userName;
+    public String password;
+    public String email;
+    public int rankingPoints;
+    public String themeName;
+}
