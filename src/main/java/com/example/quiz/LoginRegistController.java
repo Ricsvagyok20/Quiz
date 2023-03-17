5d7ba25104d3e2004aafb6a200e7835e5de4c988
Hegedűs Márk <h158512@stud.u-szeged.hu>
2023-03-17 16:01:04 +0100

    Login/Register fxml es controller kesz

---
+++ src/main/java/com/example/quiz/LoginRegistController.java
@@ -0,0 +1,43 @@
+package com.example.quiz;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextField;
+
+public class LoginRegistController {
+    @FXML
+    private TextField txtfUsername;
+
+    @FXML
+    private TextField txtfPassword;
+
+    @FXML
+    private Button btnLogin;
+
+    @FXML
+    private TextField txtfUsernameRegist;
+
+    @FXML
+    private TextField txtfEmailRegist;
+
+    @FXML
+    private TextField txtfPasswordRegist;
+
+    @FXML
+    private TextField txtfPasswordRegistAgain;
+
+    @FXML
+    private CheckBox chbUA;
+
+    @FXML
+    private Button btnRegister;
+
+
+    public void Login(ActionEvent event) {
+    }
+
+    public void register(ActionEvent event) {
+    }
+}
