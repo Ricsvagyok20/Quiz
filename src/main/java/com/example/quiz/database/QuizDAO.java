d4aa190fc2ae1d88dd4399b5b629691f24c54670
Ruck Richard <h163500@stud.u-szeged.hu>
2023-03-27 16:35:23 +0200

    Dao első add method

--- src/main/java/com/example/quiz/database/QuizDAO.java
+++ src/main/java/com/example/quiz/database/QuizDAO.java
@@ -12,6 +12,9 @@ import java.util.List;
 public class QuizDAO implements IQuizDAO {
 
     private Statement statement;
+    private Connection conn;
+    private ResultSet rs;
+    private String sql;
 
     Connection DAO(){
         try {
@@ -28,10 +31,10 @@ public class QuizDAO implements IQuizDAO {
     public List<Player> getPlayers(){
         List<Player> players = new ArrayList<>();
         try{
-            Connection conn = DAO();
+            conn = DAO();
             statement = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            String sql = "SELECT * FROM JATEKOS";
-            ResultSet rs = statement.executeQuery(sql);
+            sql = "SELECT * FROM JATEKOS";
+            rs = statement.executeQuery(sql);
             while (rs.next()){
                 Player tmp = new Player(rs.getString(1),rs.getString(2),rs.getString(3),rs.getInt(4), rs.getString(5));
                 players.add(tmp);
@@ -48,10 +51,10 @@ public class QuizDAO implements IQuizDAO {
     public List<Quiz> getQuizes() {
         List<Quiz> quizzes = new ArrayList<>();
         try{
-            Connection conn = DAO();
+            conn = DAO();
             statement = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
-            String sql = "SELECT * FROM QUIZ";
-            ResultSet rs = statement.executeQuery(sql);
+            sql = "SELECT * FROM QUIZ";
+            rs = statement.executeQuery(sql);
             while (rs.next()){
                 Quiz tmp = new Quiz(rs.getInt(1), rs.getString(2));
                 quizzes.add(tmp);
@@ -97,6 +100,21 @@ public class QuizDAO implements IQuizDAO {
     @Override
     public List<Play> getPlays() {
         return null;
+    }
+
+    public boolean addPlayer(Player player){
+        try{
+            conn = DAO();
+            statement = conn.createStatement(ResultSet.TYPE_SCROLL_SENSITIVE, ResultSet.CONCUR_READ_ONLY);
+            sql = "INSERT INTO JATEKOS " +
+                    "VALUES ('"+player.getUserName()+"', '"+player.getPassword()+"', '"+player.getEmail()+"', '"+player.getRankingPoints()+"', '"+player.getEmail()+"')";
+            rs = statement.executeQuery(sql);
+        }catch (Exception e){
+            e.printStackTrace();
+            return false;
+        }
+
+        return true;
     }
 
 }
