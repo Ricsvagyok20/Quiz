d9f7a8b5af084388389a31843d16c8d641f53f21
Hegedűs Márk <h158512@stud.u-szeged.hu>
2023-04-18 17:02:22 +0200

    fxml changes

--- src/main/java/com/example/quiz/QuestionAddController.java
+++ src/main/java/com/example/quiz/QuestionAddController.java
@@ -3,20 +3,26 @@ package com.example.quiz;
 import com.example.quiz.database.IQuizDAO;
 import com.example.quiz.database.QuizDAO;
 import com.example.quiz.modules.Question;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 
 import java.io.IOException;
+import java.net.URL;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
-public class QuestionAddController{
+public class QuestionAddController implements Initializable {
+    public ComboBox<String> cboxSubtopic;
     @FXML private TextField txtfQuestionContent;
-    @FXML private TextField txtfSubtopicName;
     @FXML private Label label;
     @FXML private Button btnCancel;
     @FXML private Button btnSave;
@@ -25,7 +31,7 @@ public class QuestionAddController{
 
     public void btnSaveAction(ActionEvent event) throws SQLException {
         String content = txtfQuestionContent.getText();
-        String subtopic = txtfSubtopicName.getText();
+        String subtopic = cboxSubtopic.getValue();
         try {
             Question question = new Question(content, subtopic);
             dao = new QuizDAO();
@@ -42,4 +48,14 @@ public class QuestionAddController{
     }
 
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        dao = new QuizDAO();
+        ObservableList<String> subtopics = FXCollections.observableArrayList();
+        var topics = dao.getSubtopics();
+        for (var i: topics){
+            subtopics.add(i.getTopicName());
+        }
+        cboxSubtopic.getItems().setAll(subtopics);
+    }
 }
